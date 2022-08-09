84878f774b03d81b7ee74450df4ce7fe413c9cbd
Riley-Bess <110930798+Riley-Bess@users.noreply.github.com>
2022-08-09 14:05:57 -0400

    Add files via upload

---
+++ Adobe FIPS Fix.ps1
@@ -0,0 +1,15 @@
+﻿"`n"
+
+Write-Host "!!!RUN UNDER USER THAT IS HAVING ISSUES PROFILE, NOT AS ADMIN!!!"
+
+"`n"
+
+Write-Host "This Script will kill Adobe Acrobat Process and set the bFIPSMode registry key to 0 to fix the Adobe FIPS issue"
+Read-Host -Prompt "Press enter to continue..."
+
+
+Stop-Process -Name "Acrobat"
+set-ItemProperty -Path "HKCU:\Software\Adobe\Adobe Acrobat\DC\AVGeneral" -Name "bFIPSMode" -Value 0
+Get-ItemProperty -Path "HKCU:\Software\Adobe\Adobe Acrobat\DC\AVGeneral" -Name "bFIPSMode"
+
+Read-Host -Prompt "bFIPSMode (confirm above) should now be set to 0.  Press Enter to Exit"
